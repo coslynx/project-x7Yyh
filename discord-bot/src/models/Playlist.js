fdb6776023b6af0d7ec5f4ab99d2f68d146b4a8b
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-17 09:50:13 +0530

    Generated: `discord-bot/src/models/Playlist.js`

---
+++ discord-bot/src/models/Playlist.js
@@ -0,0 +1,30 @@
+```javascript
+const mongoose = require('mongoose');
+
+const playlistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  songs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Song',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Playlist', playlistSchema);
+
+```
